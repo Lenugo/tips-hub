baa59120e5b350247e57d7baf800fc323d127574
lenugo <54646168+Lenugo@users.noreply.github.com>
2025-06-09 22:33:09 -0400

    fix: cors config prod

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,9 +1,18 @@
 import { envs } from './envs'
 
+const originConfig = () => {
+  let origins: string[] = []
+  if (envs.NODE_ENV !== 'production') {
+    origins = ['http://localhost:5173']
+  } else {
+    origins = ['https://lenugo.github.io', 'https://lenugo.github.io/tips-hub']
+  }
+
+  return origins.flat()
+}
+
 export const corsConfig = {
-  origin: [
-    envs.NODE_ENV === 'production' ? 'https://lenugo.github.io/tips-hub' : 'http://localhost:5173'
-  ],
+  origin: originConfig(),
   credentials: true,
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
